c432cff4062032ef2e39ac1df6db194a3f5bdb41
norby7 <hambuernorbi@yahoo.com>
2019-07-11 18:31:13 +0300

    Docker req

--- app.py
+++ app.py
@@ -765,11 +765,12 @@ def protected():
 def build_docker_image():
     try:
         client = docker.from_env()
-        print(client.images)
-        new_image = client.images.build(path="/docker_template", tag='chatbot', dockerfile='/docker_template/Dockerfile')
 
-        #ret = subprocess.run(['docker', 'save', '-o', './chatbot2.tar', 'chatbot'])
-        #print(ret)
+        new_image = client.images.build(path="./docker_template", tag='chatbot')
+
+        print("asd")
+        # ret = subprocess.run(['docker', 'save', '-o', './chatbot2.tar', 'chatbot'])
+        # print(ret)
         username = "admin sure"
         return jsonify({'hello': 'from {}'.format(username)}), 200
     except Exception as ex:
--- docker_template/app.py
+++ docker_template/app.py
@@ -1,8 +1,1 @@
-from chatterbot import ChatBot
-
-chatbot = ChatBot('Chatbot', storage_adapter="chatterbot.storage.SQLStorageAdapter", database="db.sqlite3",
-                  database_uri="sqlite:///db.sqlite3")
-
-while (True):
-    user_input = input()
-    print(chatbot.get_response(user_input))
+print("asd")
